f252a2737ca18ea3dbfa779cb4c7ce0209a0f25b
kodex_snow <kotsnow.programmer@gmail.com>
2017-06-04 15:13:51 +0200

    SyntaxHighlighter regex

--- src/kot/kotsnow/ookEditor/SyntaxHighlighter.java
+++ src/kot/kotsnow/ookEditor/SyntaxHighlighter.java
@@ -6,18 +6,58 @@ import java.util.regex.Pattern;
 public class SyntaxHighlighter {
 
 	StringBuilder text;
-	int position=0;
-	char characterAtPosition=' ';
+
+	String openTagStyle="<p style=\"color:red\">";
+	String closeTagStyle="</p\">";
 
 	Pattern pattern;
 	Matcher matcher;
 
-	public String colorSyntax(String textFromEditor){
+	String keywordRegex = "Ook([\\!\\.]\\s+Ook[\\!\\?\\.])|([\\?]\\s+Ook[\\!\\.])";
+
+	public SyntaxHighlighter(){
+		pattern=Pattern.compile(keywordRegex);
+	}
+
+	public String highlight(String textFromEditor){
 
 		text = new StringBuilder(textFromEditor);
+		deleteOpenTags();
+		deleteCloseTags();
 
 		return text.toString();
 	}
 
+	private void colorWords(){
+		matcher.find();
+
+	}
+
+	private void deleteOpenTags(){
+
+		int index=text.indexOf(openTagStyle);
+
+		while(index!=-1){
+			text.delete(index, index+openTagStyle.length());
+			index=text.indexOf(openTagStyle);
+		};
+
+	}
+
+	private void deleteCloseTags(){
+
+		int index=text.indexOf(closeTagStyle);
+
+		while(index!=-1){
+			text.delete(index, index+closeTagStyle.length());
+			index=text.indexOf(closeTagStyle);
+		};
+
+	}
+
+
+	public static void main(){
+
+	}
 
 }
